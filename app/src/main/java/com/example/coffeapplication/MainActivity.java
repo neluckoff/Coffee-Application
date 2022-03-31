47890f0ac6e371fc0c7f49715b52de308ddb55bb
luckoff <neluckoff@gmail.com>
2022-03-31 15:07:08 +0300

    New complete

--- app/src/main/java/com/example/coffeapplication/MainActivity.java
+++ app/src/main/java/com/example/coffeapplication/MainActivity.java
@@ -1,14 +1,34 @@
 package com.example.coffeapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
+    SupportMapFragment mapFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.registration);
+        setContentView(R.layout.personal_area);
+
+        /*mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);*/
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        LatLng CoofeOne = new LatLng(55.75772607568077, 37.62742044545359);
+        googleMap.addMarker(new MarkerOptions().position(CoofeOne).title("Наша точка"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(CoofeOne));
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(CoofeOne, 10f));
     }
 }
