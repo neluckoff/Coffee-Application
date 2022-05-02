d78d6ff72e9cfcb0a7ef330d7ea93a5e714bbfd7
luckoff <neluckoff@gmail.com>
2022-05-02 20:16:11 +0300

    complete QR generator in profile

--- app/src/main/java/com/example/coffeapplication/mvvm/views/PersonFragment.java
+++ app/src/main/java/com/example/coffeapplication/mvvm/views/PersonFragment.java
@@ -1,23 +1,27 @@
 package com.example.coffeapplication.mvvm.views;
 
 import android.app.Dialog;
+import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 
-import com.example.coffeapplication.MainActivity;
 import com.example.coffeapplication.R;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
 
 public class PersonFragment extends Fragment {
     Button loyaltyProgram;
@@ -25,12 +29,33 @@ public class PersonFragment extends Fragment {
     Button editProfile;
     Button historyOrders;
     Dialog loyaltyDialog;
-    BottomNavigationView bottomNavigationView;
+    ImageView qrcode;
+    Dialog ordersDialog;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_person, container, false);
+        View view = inflater.inflate(R.layout.fragment_person, container, false);
+        String data = "Name Firstname Sale";
+        qrcode = view.findViewById(R.id.QRcode);
+        MultiFormatWriter writer = new MultiFormatWriter();
+        try {
+            BitMatrix matrix = writer.encode(data, BarcodeFormat.QR_CODE, 1000, 1000);
+            BarcodeEncoder encoder = new BarcodeEncoder();
+            Bitmap bitmap = encoder.createBitmap(matrix);
+            qrcode.setImageBitmap(bitmap);
+
+        } catch (WriterException e) {
+            e.printStackTrace();
+        }
+
+        return view;
     }
 
     @Override
@@ -45,7 +70,39 @@ public class PersonFragment extends Fragment {
             public void onClick(View view) {
                 loyaltyDialog.setContentView(R.layout.loyalty_program);
                 loyaltyDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                loyaltyDialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+
+//                Button closeLoyalty = view.findViewById(R.id.materialCloseButton);
+//                closeLoyalty.setOnClickListener(new View.OnClickListener() {
+//                    @Override
+//                    public void onClick(View view) {
+//                        loyaltyDialog.dismiss();
+//                    }
+//                });
+
                 loyaltyDialog.show();
+            }
+        });
+
+        ordersDialog = new Dialog(getContext());
+        historyOrders = view.findViewById(R.id.listOrderButton);
+
+        historyOrders.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ordersDialog.setContentView(R.layout.orders_history);
+                ordersDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                ordersDialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+
+//                Button closeOrders = view.findViewById(R.id.closeHistoryBtn);
+//                closeOrders.setOnClickListener(new View.OnClickListener() {
+//                    @Override
+//                    public void onClick(View view) {
+//                        ordersDialog.dismiss();
+//                    }
+//                });
+
+                ordersDialog.show();
             }
         });
     }
